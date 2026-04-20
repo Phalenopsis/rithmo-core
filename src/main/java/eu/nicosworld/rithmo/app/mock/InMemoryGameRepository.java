cdb8fbbe9ab9dd0b5f55f7ecbb4cb19857fe0596
Nicolas Leroux <hertford.fr@gmail.com>
2026-04-20 08:08:00 +0200

    config : first commit

---
+++ src/main/java/eu/nicosworld/rithmo/app/mock/InMemoryGameRepository.java
@@ -0,0 +1,14 @@
+package eu.nicosworld.rithmo.app.mock;
+
+
+import eu.nicosworld.rithmo.core.GameRepository;
+
+/**
+ * In memory repository
+ * <p>
+ *     Stock data in memory (HashMap)
+ * </p>
+ */
+public class InMemoryGameRepository implements GameRepository {
+
+}
