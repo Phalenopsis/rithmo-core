e1af42b89857cdd659163a59a2b8d1fab265c218
Nicolas Leroux <hertford.fr@gmail.com>
2026-04-26 11:51:27 +0200

    feat : add a static method to create a PreCaptureAction from a PreCaptureChoice

--- src/main/java/eu/nicosworld/rithmo/core/turn/action/PreCaptureAction.java
+++ src/main/java/eu/nicosworld/rithmo/core/turn/action/PreCaptureAction.java
@@ -1,5 +1,6 @@
 package eu.nicosworld.rithmo.core.turn.action;
 
+import eu.nicosworld.rithmo.core.turn.resolver.PreCaptureChoice;
 import eu.nicosworld.rithmo.engine.capture.CaptureAction;
 import eu.nicosworld.rithmo.engine.model.Position;
 
@@ -8,4 +9,11 @@ import java.util.List;
 public record PreCaptureAction(
         List<CaptureAction> actions,
         Position landingOption
-) implements TurnAction {}
+) implements TurnAction {
+
+    public static PreCaptureAction from(PreCaptureChoice choice, Position landing) {
+        if (choice.landingOptions().contains(landing))
+            return new PreCaptureAction(List.copyOf(choice.actions()), landing);
+        throw new RuntimeException("Creating PreCaptureAction from PreCaptureChoice : Landing is not in landing choices.");
+    }
+}
