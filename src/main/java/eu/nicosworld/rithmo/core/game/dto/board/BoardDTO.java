c2ad8d00f9c1195dd3a5637c0664c329b2ba42ef
Nicolas Leroux <hertford.fr@gmail.com>
2026-05-01 16:06:07 +0200

    feat(BoardDTO) : add width and heigth to help for display

--- src/main/java/eu/nicosworld/rithmo/core/game/dto/board/BoardDTO.java
+++ src/main/java/eu/nicosworld/rithmo/core/game/dto/board/BoardDTO.java
@@ -5,12 +5,14 @@ import eu.nicosworld.rithmo.engine.model.PieceAtPosition;
 
 import java.util.List;
 
-public record BoardDTO(List<PieceDTO> pieces) {
+public record BoardDTO(List<PieceDTO> pieces,
+                       int width,
+                       int height) {
     public static BoardDTO mapFrom(Board board) {
         List<PieceAtPosition> piecesAtPosition = board.getPiecesWithPositions();
         List<PieceDTO> pieces = piecesAtPosition.stream()
                 .map(PieceDTO::mapFrom)
                 .toList();
-        return new BoardDTO(pieces);
+        return new BoardDTO(pieces, board.getWidth(), board.getHeight());
     }
 }
