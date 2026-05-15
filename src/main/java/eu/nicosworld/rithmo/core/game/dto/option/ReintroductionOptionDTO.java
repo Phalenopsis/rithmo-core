5717aa8fdd50b1d6a5d1febebf0af3a3342fe404
Nicolas Leroux <hertford.fr@gmail.com>
2026-05-15 17:56:37 +0200

    feat(PlayerOptionDTO) : add ReintroductionOptionDTO in PlayerOptionDTO

--- src/main/java/eu/nicosworld/rithmo/core/game/dto/option/ReintroductionOptionDTO.java
+++ src/main/java/eu/nicosworld/rithmo/core/game/dto/option/ReintroductionOptionDTO.java
@@ -4,10 +4,10 @@ import eu.nicosworld.rithmo.core.game.dto.board.PieceDTO;
 import eu.nicosworld.rithmo.core.turn.option.ReintroductionOption;
 import eu.nicosworld.rithmo.engine.model.Position;
 
-public record ReintroductionDTO(PieceDTO pieceDTO,
-                                Position landing) {
-    public static ReintroductionDTO from(ReintroductionOption option) {
-        return new ReintroductionDTO(
+public record ReintroductionOptionDTO(PieceDTO pieceDTO,
+                                      Position landing) implements PlayerOptionDTO {
+    public static ReintroductionOptionDTO from(ReintroductionOption option) {
+        return new ReintroductionOptionDTO(
                 PieceDTO.from(option.reintroduction().piece(), null),
                 option.reintroduction().position());
     }
