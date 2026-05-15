dbaa3565213dc1d4b4f2172ee10e712c56f2049b
Nicolas Leroux <hertford.fr@gmail.com>
2026-05-15 09:46:33 +0200

    doc(PreCaptureOption): add javadoc

--- src/main/java/eu/nicosworld/rithmo/core/turn/option/LandingOption.java
+++
@@ -1,6 +0,0 @@
-package eu.nicosworld.rithmo.core.turn.option;
-
-import javax.swing.text.Position;
-
-public record LandingOption(Position landing) {
-}
--- src/main/java/eu/nicosworld/rithmo/core/turn/option/PreCaptureOption.java
+++ src/main/java/eu/nicosworld/rithmo/core/turn/option/PreCaptureOption.java
@@ -5,6 +5,21 @@ import eu.nicosworld.rithmo.engine.model.Position;
 
 import java.util.List;
 
+/**
+ * Represents a pre-move capture option available during a turn.
+ * <p>
+ * A pre-capture option contains:
+ * <ul>
+ *     <li>the list of capture actions performed as part of the option</li>
+ *     <li>the possible landing positions where captured pieces may be redeployed</li>
+ * </ul>
+ *
+ * @param captures
+ *         capture actions included in this option
+ *
+ * @param possibleLandings
+ *         valid landing positions for captured-piece redeployment
+ */
 public record PreCaptureOption(
         List<CaptureAction> captures,
         List<Position> possibleLandings
