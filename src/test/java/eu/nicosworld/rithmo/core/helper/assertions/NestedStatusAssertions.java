f82e2e89d8760e1bd7d70d09f9b48be32d5d4086
Nicolas Leroux <hertford.fr@gmail.com>
2026-05-27 21:53:33 +0200

    refactor(assertions): add hub navigation

--- src/test/java/eu/nicosworld/rithmo/core/helper/assertions/NestedStatusAssertions.java
+++ src/test/java/eu/nicosworld/rithmo/core/helper/assertions/NestedStatusAssertions.java
@@ -5,18 +5,38 @@ import eu.nicosworld.rithmo.core.helper.StatusDTOAssertion;
 
 abstract class NestedStatusAssertions {
 
-    protected final GameStatusDTO status;
+    protected final GameStatusDTO actual;
     protected final StatusDTOAssertion parent;
 
     protected NestedStatusAssertions(
-            GameStatusDTO status,
+            GameStatusDTO actual,
             StatusDTOAssertion parent
     ) {
-        this.status = status;
+        this.actual = actual;
         this.parent = parent;
     }
 
     public StatusDTOAssertion and() {
         return parent;
+    }
+
+    public GlobalAssertions status() {
+        return parent.status();
+    }
+
+    public DecisionAssertions decisions() {
+        return parent.decisions();
+    }
+
+    public OptionAssertions options() {
+        return parent.options();
+    }
+
+    public AssetAssertions assets() {
+        return parent.assets();
+    }
+
+    public BoardAssertions board() {
+        return parent.board();
     }
 }
