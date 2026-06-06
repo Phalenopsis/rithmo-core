122494486e63243b0daf56afabc907dfbaf7e7cb
Nicolas Leroux <hertford.fr@gmail.com>
2026-06-06 20:31:16 +0200

    feat(game): add configurable victory conditions

--- src/main/java/eu/nicosworld/rithmo/core/GameOptions.java
+++ src/main/java/eu/nicosworld/rithmo/core/GameOptions.java
@@ -1,9 +1,21 @@
 package eu.nicosworld.rithmo.core;
 
 import eu.nicosworld.rithmo.core.game.CaptureRuleOption;
-import eu.nicosworld.rithmo.core.game.VictoryRuleOption;
+import eu.nicosworld.rithmo.core.game.victory.VictoryCondition;
+import eu.nicosworld.rithmo.engine.victory.VictoryType;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public record GameOptions(
-    Set<CaptureRuleOption> captureRules, Map<VictoryRuleOption, Integer> victoryRules) {}
+    Set<CaptureRuleOption> captureRules,
+    Map<VictoryType, Integer> victoryRules,
+    Set<VictoryCondition> victoryConditions) {
+
+  public GameOptions(Set<CaptureRuleOption> captureRules, Map<VictoryType, Integer> victoryRules) {
+    this(
+        captureRules,
+        victoryRules,
+        victoryRules.keySet().stream().map(VictoryCondition::fromRule).collect(Collectors.toSet()));
+  }
+}
